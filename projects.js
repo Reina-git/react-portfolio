d6e0718afc9a96b53826d213c4bc7017bafc4083
Reina Uhler <reinadu@gmail.comgit>
2020-11-03 17:44:20 -0800

    updated formatting

---
+++ projects.js
@@ -0,0 +1,27 @@
+const projects = [
+   {
+      id: "ceee362e-1d4c-11eb-adc1-0242ac120002",
+      title: "Validation of Email and Password on Account Creation",
+      rating: 4,
+      desc: "Password and email validation following best practices.",
+      totalMinutes: 3,
+      postedAt: 1604351580776,
+      githubUrl: "https://github.com/Reina-git/Crowd-tag",
+      image: "validationOfEmailAndPasswordOnAccountCreation.webp",
+      youtubeUrl: "https://www.youtube.com/watch?v=7upUHKgsLLI",
+      isActive: true,
+   },
+   {
+      id: "7577e664-289a-4adc-856b-bfaf87baac06",
+      title: "Walkthrough of Crowd Tag prototype",
+      rating: 4,
+      desc:
+         "Prototype of Crowd Tag, an app that helps libraries crowdsource metadata for their photographic collections.",
+      totalMinutes: 3,
+      postedAt: 1604351580776,
+      githubUrl: "https://github.com/Reina-git/Crowd-tag",
+      image: "walkthroughOfCrowdTagPrototype.webp",
+      youtubeUrl: "https://www.youtube.com/watch?v=LN02lBrDdt4&t=1s",
+      isActive: true,
+   },
+];
